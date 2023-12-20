6fce9534cd3ced8d743d8adc8fe065c07c4ea8f9
semih <semihcaliskan0103@gmail.com>
2023-12-20 16:15:24 +0300

    init

---
+++ src/main/java/com/land/quartzschedulerdemo/QuartzSchedulerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.land.quartzschedulerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzSchedulerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzSchedulerDemoApplication.class, args);
+    }
+
+}
